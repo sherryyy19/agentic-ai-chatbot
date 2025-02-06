4166a9e0f7c761020f8f2f6b509236ec222992f5
Sharjeel Ahmed <ahmedsharjeel758@gmail.com>
2025-02-06 13:54:58 -0700

    Update ai_agent.py

--- ai_agent.py
+++ ai_agent.py
@@ -4,14 +4,14 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 
-# Step1 --> Setup API Keys for Groq, OpenAI, and Tavily
+# Step-1 --> Setup API Keys for Groq, OpenAI, and Tavily
 import os
 
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-# Step2 --> Setup LLM & Tools
+# Step-2 --> Setup LLM & Tools
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -21,7 +21,7 @@ groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
 
 search_tool = TavilySearchResults(max_results=2)
 
-# Step3: Setup AI Agent with Search tool functionality
+# Step-3 --> Setup AI Agent with Search tool functionality
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages.ai import AIMessage
 
